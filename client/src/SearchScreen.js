99dbc1717c2ae2289ba41cd6adca6b5a328e6be9
salamandermartin <salamancam.programming@gmail.com>
2021-01-23 23:13:00 -0500

    :)

---
+++ client/src/SearchScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "react-native-gesture-handler";
+import { StyleSheet, Text, View, TouchableOpacity, Image} from "react-native";
+
+const SearchScreen = ({ navigation }) => {
+  return (
+    <Text> yee </Text>
+  );
+};
+
+export default SearchScreen;
