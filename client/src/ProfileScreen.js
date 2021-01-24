8f3a0a6f21a7724aaaa7ef546cc44753d4756d77
salamanderm <salamancam.programming@gmail.com>
2021-01-24 04:34:06 -0500

    cache development files

--- client/src/ProfileScreen.js
+++ client/src/ProfileScreen.js
@@ -3,12 +3,22 @@ import "react-native-gesture-handler";
 import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
 
 class ProfileScreen extends Component {
+  state = {
+    global_avg_comparison: '0%',
+    app_avg_comparison:'0%',
+  }
+  global_avg_set = (percent) => {
+    this.setState({ food : percent })
+  }
+  app_avg_set = (percent) => {
+    this.setState({ food : percent })
+  }
 
   render() {
     return (
       <View style={styles_ps.body}>
-        <View>
-
+        <View style={styles_ps.top_percentages_container}>
+          <Text style={styles_ps.top_percentages}></Text>
 
         </View>
       </View>
@@ -20,7 +30,11 @@ export default ProfileScreen;
 
 const styles_ps = StyleSheet.create({
   body: {
-    backgroundColor: '#D8D8D6',
+    backgroundColor: '#D5B9B1',
     flexDirection: 'column',
-  }
+  },
+  top_percentages: {
+    fontSize: 28,
+    fontWeight: 'bold',
+  },
 })
