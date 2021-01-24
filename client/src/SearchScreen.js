2f9737ffa562ab94e64184c4d508441c419f98a4
JuZNyC <junz.nyc@gmail.com>
2021-01-24 11:36:51 -0500

    SearchScreen

--- client/src/SearchScreen.js
+++ client/src/SearchScreen.js
@@ -1,11 +1,102 @@
-import React from "react";
-import "react-native-gesture-handler";
-import { StyleSheet, Text, View, TouchableOpacity, Image} from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+import { View, FlatList, ActivityIndicator } from 'react-native';
+import { ListItem, SearchBar } from 'react-native-elements';
+import FoodEmission from './foodemissions.json'
 
-const SearchScreen = ({ navigation }) => {
-  return (
-    <Text> yee </Text>
-  );
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      loading: false,
+      data: [],
+      error: null,
+    };
+
+    this.arrayholder = [];
+  }
+
+  componentDidMount() {
+    this.makeRemoteRequest();
+  }
+
+  makeRemoteRequest = () => {
+    this.setState({
+      data: FoodEmission,
+      loading: false,
+    });
+    this.arrayholder = FoodEmission;
+  };
+
+  renderSeperator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: '86%',
+          backgroundColor: '#CED0CE',
+          marginLeft: '14%',
+        }}
+      />
+    );
+  };
+
+  searchFilterFunction = text => {
+    this.setState({
+      value: text,
+    });
+    const newData = this.arrayholder.filter(item => {
+      const itemData = `${item.name.toUpperCase()} `;
+      const textData = text.toUpperCase();
+      console.log(textData)
+      return itemData.indexOf(textData) > -1;
+    });
+    this.setState({
+      data: newData,
+    });
+  };
+
+
+  renderHeader = () => {
+    return (
+      <SearchBar
+        placeholder="Type Here..."
+        lightTheme
+        round
+        onChangeText={text => this.searchFilterFunction(text)}
+        autoCorrect={false}
+        value={this.state.value}
+      />
+    );
+  };
+
+  render() {
+    if (this.state.loading) {
+      return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+    return (
+      <View style={{ flex: 1 }}>
+        <FlatList
+          data={this.state.data}
+          renderItem={({ item }) => (
+            <ListItem bottomDivider >
+              <ListItem.Content>
+                <ListItem.Title>{item.name}</ListItem.Title>
+                <ListItem.Subtitle>{item.emission}</ListItem.Subtitle>
+              </ListItem.Content>
+            </ListItem>
+          )}
+          ItemSeparatorComponent={this.renderSeparator}
+          ListHeaderComponent={this.renderHeader}
+        />
+      </View>
+    );
+  }
+}
 
 export default SearchScreen;
